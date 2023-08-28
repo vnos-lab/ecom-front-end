c6c5288b35189fb0be187db94e66d985a4e966c1
Charlie Nguyen <vawntanng@gmail.com>
2023-08-29 00:49:25 +0700

    [REFACTOR] refactor code with eslint

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 import AppButton from "@/components/App/AppButton";
 
+import SectionService from "@/components/App/SectionService";
+
 import FlashSales from "./home/FlashSales";
 import HeroBanner from "./home/HeroBanner";
-import SectionService from "@/components/App/SectionService";
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
       <div className="h-[1000px]" />
 
       <FlashSales />
-      <SectionService/>
+      <SectionService />
     </main>
   );
 }
