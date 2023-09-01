ed3e502743b583e15bbb0a5fe1897f0d4fc21d01
Charlie Nguyen <vawntanng@gmail.com>
2023-09-01 17:13:29 +0700

    [ADD] add logic button scroll to top and add z-index to header

--- components/App/ButtonScrollTop/index.jsx
+++ components/App/ButtonScrollTop/index.jsx
@@ -15,13 +15,13 @@ function ScrollToTop() {
   };
   if (visibleButtonScrollTop) {
     return (
-      // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
-      <div
+      <button
+        type="button"
         className="fixed bottom-[25%] z-10 right-[46px] flex items-center justify-center w-[46px] h-[46px] rounded-full bg-[#F5F5F5] cursor-pointer ease-in-out duration-300"
         onClick={onScrollTop}
       >
         <Icons.ArrowUp size={24} />
-      </div>
+      </button>
     );
   }
 }
